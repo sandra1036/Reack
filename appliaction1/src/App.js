23f64fc0b5c23bd93267899cb29baeb39b7ab322
Sandra <sandraromero262@gmail.com>
2020-03-12 15:52:14 +0100

    Primera aplicacion

---
+++ appliaction1/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+//import './AppS2.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Bienvenidos a este curso</h2>
+        <h3>Vamos ha aprender React</h3>
+        <p>Hola mundo estoy utilizando React</p>
+        <strong>Esto es un Strong</strong>
+        <button className="App-button">Hola soy un boton</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener  noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
