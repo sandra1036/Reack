c3c41bb47366145c65309de096aa1bf85cc722f5
Sandra <sandraromero262@gmail.com>
2020-03-16 13:40:31 +0100

    Children layout

--- children/src/App.js
+++ children/src/App.js
@@ -1,6 +1,21 @@
 
 import React, { Component } from 'react';
 
+
+class Article extends Component{
+  render(){
+    return(
+      <section>
+        <h2>{this.props.title}</h2>
+        <p><em>Escrito por {this.props.author}</em></p>
+        <Box>{this.props.date}</Box>
+        <article>
+          {this.props.children}
+        </article>
+      </section>
+    )
+  }
+}
 class Box extends Component{
   render(){
     return(
@@ -17,8 +32,20 @@ class App extends Component {
     return(
       <div className='App'>
         <h4>Children props</h4>
-        <Box>Hola! Soy un children!</Box>
-        <Box>Otro Box con otro contenido</Box>
+        <Article
+          author='Sandra'
+          date={new Date().toLocaleDateString()}
+          title='Artículo sobre la prop children'>
+            <p>El contenido que envolvemos dentro del componente Article será enviado</p>
+            <strong>Y mantiene las etiquetas y componentes</strong>
+          </Article>
+          <Article
+          author='Sandra'
+          date={new Date().toLocaleDateString()}
+          title='Artículo 2'>
+            <p>El contenido que envolvemos dentro del componente Article será enviado</p>
+            <strong>Y mantiene las etiquetas y componentes</strong>
+          </Article>
         </div>
     )
   }
