bf4011f3fad9e268c5e109e3b7b8c7a555614fbb
Sandra <sandraromero262@gmail.com>
2020-03-16 12:27:53 +0100

    Formulario como no se deberia hacer

---
+++ formulario/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Forms from './sections/forms'
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Forms/>
+      </div>
+    );
+  }
+}
+export default App;
