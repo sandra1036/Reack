4f39c13f11f9d7adb01d2808643c05c9339d2d8c
Sandra <sandraromero262@gmail.com>
2020-03-16 12:05:23 +0100

    Eventos mover Raton

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor (){
+    super()
+    this.state={ mouseX:0,mouseY:0 }
+    //this.handleMouseMove=this.handleMouseMove.bind(this)
+  }
 
+
+  handleMouseMove=(e) => {
+    const { clientX,clientY }=e
+    this.setState({ mouseX:clientX,mouseY:clientY })
+  }
   handleClick(e) {
     console.log(e)
     console.log(e.nativeEvent)
@@ -12,6 +22,11 @@ class App extends Component {
       <div className="App">
         <h4>Eventos</h4>
         <button onClick={this.handleClick}>Hi there!</button>
+        <div
+          onMouseMove={this.handleMouseMove}
+          style={{border:'1px solid #000',marginTop:10,padding:10}}>
+            <p>{this.state.mouseX},{this.state.mouseY}</p>
+        </div>
       </div>
     );
   }
