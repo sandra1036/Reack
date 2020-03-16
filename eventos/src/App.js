7cd8b3075a6fa009a573a177125e40f5d21cc875
Sandra <sandraromero262@gmail.com>
2020-03-16 11:45:47 +0100

    Evento de boton Alert

---
+++ eventos/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Eventos</h4>
+      <button onClick={()=>alert('Hi here!')}>Hi there!</button>
+    </div>
+  );
+}
+
+export default App;
