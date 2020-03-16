670e45b7e975a3e4e97f275f157f4efff38df7ba
Sandra <sandraromero262@gmail.com>
2020-03-16 10:29:00 +0100

    States

--- state/src/App.js
+++ state/src/App.js
@@ -3,9 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class Contador extends Component{
-  constructor(){
-    super()
-    this.state={contador:1}
+  constructor(props){
+    super(props)
+    console.log(this.props.contadorInicial)
+    this.state={contador:this.props.contadorInicial}
     setInterval(()=>{
       this.setState({contador:this.state.contador +1})
     },1000)
@@ -15,6 +16,12 @@ class Contador extends Component{
     return <ContadorNumero numero={this.state.contador}/>
   }
 }
+
+Contador.defaultProps={
+  contadorInicial: 0
+}
+
+
 class ContadorNumero extends Component{
   render(){
     console.log('ContadorNumero render()')
@@ -26,7 +33,7 @@ function App() {
   return (
     <div className="App">
       <p>Primer componente con state</p>
-      <Contador/>
+      <Contador contadorInicial={100}/>
     </div>
   );
 }
