5f32b3c86d9e6fc7666702b75c298d1fc03196a9
Sandra <sandraromero262@gmail.com>
2020-03-16 13:58:52 +0100

    PropTypes y Required

--- children/src/App.js
+++ children/src/App.js
@@ -1,16 +1,22 @@
 
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types'
 
 class Article extends Component{
+  static propTypes={
+    author:PropTypes.string.isRequired
+  }
+ 
   render(){
+    
+    const {author,children,date,title}=this.props
     return(
       <section>
-        <h2>{this.props.title}</h2>
-        <p><em>Escrito por {this.props.author}</em></p>
-        <Box>{this.props.date}</Box>
+        <h2>{title}</h2>
+        {author && <p><em>Escrito por {author}</em></p>}
+        <Box>{date}</Box>
         <article>
-          {this.props.children}
+          {children}
         </article>
       </section>
     )
@@ -40,7 +46,7 @@ class App extends Component {
             <strong>Y mantiene las etiquetas y componentes</strong>
           </Article>
           <Article
-          author='Sandra'
+          
           date={new Date().toLocaleDateString()}
           title='Artículo 2'>
             <p>El contenido que envolvemos dentro del componente Article será enviado</p>
