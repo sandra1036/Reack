cb5dbeb7bce45bfc4150769e055d76e82f2cfbae
Sandra <sandraromero262@gmail.com>
2020-03-17 13:05:25 +0100

    Terminado el fetch

--- peliculas/src/components/SearchForm.js
+++ peliculas/src/components/SearchForm.js
@@ -1,36 +1,47 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
-export class SearchForm extends Component{
-    state={
-        inputMovie:''
-    }
-    _handleChange=(e)=>{
-        this.setState({inputMovie:e.target.value})
-    }
-    _handleSubmit=(e)=>{
-        e.preventDefault()
-        alert(this.state.inputMovie)
-    }
-    render(){
-        return(
-            <form onSubmit={this._handleSubmit}>
-                <div className="field has-addons">
-                    <div className="control">
-                        <input 
-                            className="input"
-                            onChange={this._handleChange}
-                            type="text"
-                            placeholder="Search movie"/>
+const API_KEY = '4287ad07'
 
-                    </div>
-                    <div className="control">
-                        <button className="button is-info">
-                            Search
-                        </button>
+export class SearchForm extends Component {
+  state = {
+    results:[]
+  }
 
-                    </div>
-                </div>
-            </form>
-        )
-    }
+  _handleChange = (e) => {
+    this.setState({ inputMovie: e.target.value })
+  }
+
+  _handleSubmit = (e) => {
+    e.preventDefault()
+    const { inputMovie } = this.state
+
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+      .then(res => res.json())
+      .then(results => {
+        const { Search = [], totalResults = "0" } = results
+        console.log({ Search, totalResults })
+        this.props.onResults(Search)
+      })
+  }
+
+  render () {
+    return (
+      <form onSubmit={this._handleSubmit}>
+        <div className="field has-addons">
+          <div className="control">
+            <input
+              className="input"
+              onChange={this._handleChange}
+              type="text"
+              placeholder="Movie to search..." />
+          </div>
+          <div className="control">
+            <button className="button is-info">
+              Search
+            </button>
+          </div>
+        </div>
+      </form>
+    )
+  }
 }
