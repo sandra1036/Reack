cb54b998b28a33ff022e40a4ba5fb8e1c24d3ff3
Sandra <sandraromero262@gmail.com>
2020-03-17 12:43:50 +0100

    Haciendo el fetch

---
+++ peliculas/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bulma/css/bulma.css'
+import {Title}from './components/Title'
+import {SearchForm} from './components/SearchForm'
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Title>Search Movies</Title>
+        <div className="SearchForm-wrapper">
+          <SearchForm/>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
